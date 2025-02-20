342b4d8234641d5c6b6c149651f73582373c2125
holos <holosnikitatt@gmail.com>
2025-02-20 09:32:31 +0200

    Update main.rs

--- main.rs
+++ main.rs
@@ -2,19 +2,19 @@ use sysinfo::{System};
 use colored::*;
 
 fn main() {
-	println!("{}", " ##  RCH".blue());
-	println!("{}", "#  # rch".blue());
-	println!("{}", "#### RCH".blue());
-	println!("{}", "#  # rch".blue());
-	println!("{}", "#  # RCH".blue());
+	println!("{}", " ##  ####  ### #  #".blue());
+	println!("{}", "#  # #  # #    #  #".blue());
+	println!("{}", "#### #### #    ####".blue());
+	println!("{}", "#  # ###  #    #  #".blue());
+	println!("{}", "#  # # ##  ### #  #".blue());
 
 	let mut sys = System::new_all();
 
 	sys.refresh_all();
 
 	println!("=> system:");
-	println!("System name:             {:?}", System::name());
-	println!("System kernel version:   {:?}", System::kernel_version());
-	println!("System OS version:       {:?}", System::os_version());
-	println!("System host name:        {:?}", System::host_name());
+	println!("󰇄 System name:             {:?}", System::name());
+	println!(" System kernel version:   {:?}", System::kernel_version());
+	println!("󰇅 System OS version:       {:?}", System::os_version());
+	println!(" System host name:        {:?}", System::host_name());
 }
