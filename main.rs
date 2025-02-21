337def9e04f1fa62a7085698804daec077b51c64
holos <holosnikitatt@gmail.com>
2025-02-21 17:36:03 +0200

    Update main.rs

--- main.rs
+++ main.rs
@@ -1,20 +1,20 @@
+use std::process::Command;
 use sysinfo::{System};
 use colored::*;
 
 fn main() {
-	println!("{}", " ##  ####  ### #  #".blue());
-	println!("{}", "#  # #  # #    #  #".blue());
-	println!("{}", "#### #### #    ####".blue());
-	println!("{}", "#  # ###  #    #  #".blue());
-	println!("{}", "#  # # ##  ### #  #".blue());
-
-	let mut sys = System::new_all();
-
-	sys.refresh_all();
-
-	println!("=> system:");
-	println!("󰇄 System name:             {:?}", System::name());
-	println!(" System kernel version:   {:?}", System::kernel_version());
-	println!("󰇅 System OS version:       {:?}", System::os_version());
-	println!(" System host name:        {:?}", System::host_name());
+    let mut sys = System::new_all();
+    println!("{}", "L       III  N   N  U   U  X   X".yellow());
+    println!("{}", "L        I   NN  N  U   U   X X".yellow());
+    println!("{}", "L        I   N N N  U   U    X".yellow());
+    println!("{}", "L        I   N  NN  U   U   X X".yellow());
+    println!("{}", "LLLLL   III  N   N  UUUUU  X   X".yellow());
+    println!("-------------------------");
+    Command::new("whoami")
+        .spawn();
+    sys.refresh_all();
+    println!("------------------");
+    println!("{}{:?}", "OS:             ".yellow(), System::name());
+    println!("{}{:?}", "Kernel:         ".yellow(), System::kernel_version());
+    println!("{}{:?} (Seconds)", "Uptime:         ".yellow(), System::uptime());
 }
